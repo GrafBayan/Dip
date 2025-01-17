5c527fe5f51281240a947f0c12fdc336629435a0
GrafBayan <sashasashavasua@mail.ru>
2025-01-17 23:30:35 +0300

    The job is done!

--- Dip/Table/urls.py
+++ Dip/Table/urls.py
@@ -3,7 +3,7 @@ from .views import game_page, ches_page, chess_page, start_game, close_room
 
 urlpatterns = [
     path('ches/', ches_page, name='ches_page'),
-    path('game/', game_page, name='game_page'),
+    path('game/', game_page, name='game_list'),
     path('chess/<int:room_id>/', chess_page, name='chess_page'),
     path('start_game/', start_game, name='start_game'),
     path('close_room/<int:room_id>/', close_room, name='close_room'),
